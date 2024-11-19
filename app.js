9b163c211adb2e7ceeb2616399897ff694428e0f
D'Breeze <sunshinellanera32@gmail.com>
2024-11-20 02:34:16 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,96 @@
-document.querySelectorAll('.menu-item button').forEach(button => {
-    button.addEventListener('click', () => {
-        const foodItem = button.parentElement; // Get the parent menu item
-        const foodName = foodItem.querySelector('h2').textContent; // Get the food name
-        alert(`You have ordered ${foodName}!`); // Show alert with the food name
-    });
+var imageno = 1;
+displayimg(imageno);
+
+function nextimg(n) {
+    displayimg(imageno += n)
+}
+
+function currentSlide(n) {
+    displayimg(imageno = n)
+}
+
+function displayimg(n) {
+    var i;
+    var image = document.getElementsByClassName("image");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > image.length) {
+        imageno = 1;
+    }
+
+    if (n < 1) {
+        imageno = image.length;
+    }
+
+    for (i = 0; i < image.length; i++) {
+        image[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    image[imageno - 1].style.display = "block";
+    dots[imageno - 1].className += " active";
+
+}
+
+
+
+
+$(function () {
+    $(".toggle").on("click", function () {
+        if ($(".menu").hasClass("active")) {
+            $(".menu").removeClass("active");
+            $(this).find("a").html("<ion-icon name='menu-outline'></ion-icon>");
+        } else {
+            $(".menu").addClass("active");
+            $(this).find("a").html("<ion-icon name='close-outline'></ion-icon>");
+        }
+    })
 });
 
 
 
 
+
+var imageno = 1;
+displayimg(imageno);
+
+function nextimg(n) {
+    displayimg(imageno += n);
+}
+
+function currentSlide(n) {
+    displayimg(imageno = n);
+}
+
+function displayimg(n) {
+    var i;
+    var image = document.getElementsByClassName("image");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > image.length) {
+        imageno = 1;
+    }
+
+    if (n < 1) {
+        imageno = image.length;
+    }
+
+    for (i = 0; i < image.length; i++) {
+        image[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    image[imageno - 1].style.display = "block";
+    dots[imageno - 1].className += " active";
+}
+
+// Auto change images every 3 seconds
+setInterval(function () {
+    nextimg(1);
+}, 3000); // Change 3000 to any number of milliseconds you want for the interval
