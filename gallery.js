ba48f11baba18f62232627f45568b5ed6d6d6e94
D'BreezeRestaurant <sunshinellanera32@gmail.com>
2024-12-27 10:35:35 +0800

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -14,3 +14,29 @@ span.onclick = function () {
     modal.style.display = "none";
 }
 // end part is modal function
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const fadeInElements = document.querySelectorAll(".fade-in");
+
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("visible");
+                    observer.unobserve(entry.target); // Stop observing after animation
+                }
+            });
+        },
+        {
+            threshold: 0.1, // Trigger when 10% of the element is visible
+        }
+    );
+
+    fadeInElements.forEach((element) => {
+        observer.observe(element);
+    });
+});
